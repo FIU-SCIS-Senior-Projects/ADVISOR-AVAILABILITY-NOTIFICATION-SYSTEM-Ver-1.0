0bd3dc8deaf40d5bd857cc1991f1e68da0480ca2
hborg004 <hborg004@fiu.edu>
2016-12-09 13:32:23 -0500

    removed test file

--- playsoundtest.php
+++
@@ -1,9 +0,0 @@
-<audio id="sound1" src="nice-cut.mp3" preload="auto"></audio>
-
-<?php
-
-
-  echo "<script> document.getElementById('sound1').play(); </script>";
-  
-// header("Location: desk.php");
-  ?>
