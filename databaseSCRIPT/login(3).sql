700e9451dacc80cc6e2c060601c5dacbe55033a2
hborg004 <hborg004@fiu.edu>
2016-12-09 13:43:18 -0500

    updated database

--- databaseSCRIPT/login(3).sql
+++ databaseSCRIPT/login(3).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2016 at 10:29 AM
+-- Generation Time: Dec 09, 2016 at 07:38 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -27,23 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `apointments` (
-  `FirstName` varchar(100) NOT NULL,
-  `LastName` varchar(100) NOT NULL,
-  `panther_id` int(7) NOT NULL,
-  `Advisor` varchar(100) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `pid` int(7) NOT NULL,
+  `Advisor` varchar(50) NOT NULL,
   `arrival_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `appointment_time` varchar(10) NOT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `apointments`
---
-
-INSERT INTO `apointments` (`FirstName`, `LastName`, `panther_id`, `Advisor`, `arrival_time`, `appointment_time`, `id`) VALUES
-('drg', 'erte', 0, 'eter', '2016-11-30 07:16:36', 'etert', 23),
-('ryry', 'rtyrty', 0, 'marSanch004', '2016-11-30 09:18:02', 'rtytry', 24),
-('tyutyutyu', 'tyuytu', 0, 'ANY', '2016-11-30 09:23:40', 'tyutyu', 25);
 
 -- --------------------------------------------------------
 
@@ -52,15 +43,16 @@ INSERT INTO `apointments` (`FirstName`, `LastName`, `panther_id`, `Advisor`, `ar
 --
 
 CREATE TABLE `login_details` (
-  `id` varchar(15) NOT NULL,
-  `FirstName` varchar(20) NOT NULL,
-  `LastName` varchar(20) NOT NULL,
+  `id` varchar(50) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `level` int(10) NOT NULL,
   `logged` tinyint(1) NOT NULL,
   `Status` varchar(20) NOT NULL,
   `student_sent` int(1) NOT NULL,
   `student_name` varchar(100) NOT NULL,
+  `student_id` int(7) NOT NULL,
   `lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -68,17 +60,21 @@ CREATE TABLE `login_details` (
 -- Dumping data for table `login_details`
 --
 
-INSERT INTO `login_details` (`id`, `FirstName`, `LastName`, `password`, `level`, `logged`, `Status`, `student_sent`, `student_name`, `lastupdate`) VALUES
-('admin1', 'admin1', 'admin1', '$2y$10$bMMnzE8kWVqddavlnn41beZKikh1loXWYN35V2U3pUXQp/3YGX0oa', 1, 1, '', 0, '', '2016-11-30 07:35:44'),
-('admin2', 'admin2', 'admin2', '$2y$10$t45ve7OkX6QYw5FfON8xLufc/cSt/yx9mWhLcovnBGg0tOgiGXKCa', 1, 0, '', 0, '', '2016-11-29 01:33:17'),
-('advisor1', 'Advisor', 'One', '$2y$10$5KRV2WxrXKFl/EzC5fj4WOZOKDuaEXwRmoA0soXf/3UNM2NLhsJai', 0, 0, 'RFA', 0, '5 5', '2016-11-30 07:35:34'),
-('Carmen S.', 'Carmen ', 'Schenck', '$2y$10$sf4f1N3qwQzzOj448IRX1uMY1Hvk6B9qXKYl8uG/ZY4RQ2GTMMHVW', 0, 0, '', 0, '', '2016-11-21 19:36:40'),
-('display', 'display', 'queues', '$2y$10$EIgTLgkURORnnlaCK8FQtOxBZTo0Fp/pZRpqyb3Fu5SlQnCE7T7de', 3, 0, '', 0, '', '2016-11-30 07:37:56'),
-('FD1', 'Front', 'Desk', '$2y$10$oszNS0Er9zZVtCejxpUy8OZqGxgn26viNO2lXgYW3qKQcON7fungO', 2, 1, '', 0, '', '2016-11-30 08:13:55'),
-('hectom8', 'Hector', 'Borges', '$2y$10$Rgv8WO2UBQjRkQie52F96O94.dN44IX3SeL7Dhora1QC1UUUj7RYa', 0, 0, 'Busy', 0, 'Roger Smith', '2016-11-30 06:53:42'),
-('marSanch004', 'Mario', 'Sanchez', '$2y$10$IYJ3IzcqRC241EfJtGIbROO3cPaYHEPS4BJxf8f5.tdn7c33MaMla', 0, 1, 'Busy', 0, 'lol lol', '2016-11-30 05:33:30'),
-('StudentSignIn', 'student', 'sign in', '$2y$10$TzEoTYrEopab6XhJmf9ImOEiVkbh2k3y8Qq0zpehtlSdvpygMzFie', 4, 0, '', 0, '', '2016-11-30 07:51:51'),
-('SysAdm', 'System', 'Administrator', '$2y$10$.UAX0Kej.7bjNnbtvwfCB.ySUjwMj0y3Fc5vZTzXCf370iRNfk6dm', 1, 0, '', 0, '', '2016-11-21 19:40:10');
+INSERT INTO `login_details` (`id`, `FirstName`, `LastName`, `password`, `level`, `logged`, `Status`, `student_sent`, `student_name`, `student_id`, `lastupdate`) VALUES
+('admin1', 'admin1', 'admin1', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 1, 1, '', 0, '', 0, '2016-12-09 16:30:55'),
+('admin2', 'admin2', 'admin2', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 1, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('aramjohn', 'Alyssa', 'Ramjohn', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('cmullerk', 'Carmen', 'Muller Karger', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('display', 'display', 'queues', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 3, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('FD1', 'Front', 'Desk', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 2, 1, '', 0, '', 0, '2016-12-09 16:32:57'),
+('galarzal', 'Luis', 'Galarza', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('joasanab', 'JoAnna', 'Sanabria', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('lbrown', 'Lynneah', 'Brown', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('nawedder', 'Natasha', 'Wedderburn', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('sanchem', 'Mario', 'Sanchez', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 1, 'OO', 0, 'No One', 0, '2016-12-09 16:30:55'),
+('schenckc', 'Carmen', 'Schenck', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 0, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('StudentSignIn', 'student', 'sign in', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 4, 0, '', 0, '', 0, '2016-12-09 16:30:55'),
+('SysAdm', 'System', 'Administrator', '$2y$10$VIWAuf3p/r15w09hE54aEeafGSukgLzH3feoDi6vavJFFTEPajqiG', 1, 0, '', 0, '', 0, '2016-12-09 16:30:55');
 
 -- --------------------------------------------------------
 
@@ -97,7 +93,7 @@ CREATE TABLE `msgs` (
 --
 
 INSERT INTO `msgs` (`id`, `msg`, `ts_update`) VALUES
-(1, 'RFA', '2016-11-30 07:18:45');
+(1, 'RFW', '2016-12-09 03:32:21');
 
 -- --------------------------------------------------------
 
@@ -106,12 +102,12 @@ INSERT INTO `msgs` (`id`, `msg`, `ts_update`) VALUES
 --
 
 CREATE TABLE `walk_in` (
-  `FirstName` varchar(11) NOT NULL,
-  `LastName` varchar(11) NOT NULL,
-  `Advisor` varchar(11) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Advisor` varchar(50) NOT NULL,
   `ts_updte` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id` int(11) NOT NULL,
-  `pid` int(11) NOT NULL
+  `pid` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -119,10 +115,7 @@ CREATE TABLE `walk_in` (
 --
 
 INSERT INTO `walk_in` (`FirstName`, `LastName`, `Advisor`, `ts_updte`, `id`, `pid`) VALUES
-('lol', 'lol', 'lol', '2016-11-30 03:12:56', 134578, 0),
-('tyu', 'tyty', 'rtytry', '2016-11-30 03:32:53', 134579, 0),
-('rtyry', 'rrtyrty', 'ryrty', '2016-11-30 03:33:01', 134580, 0),
-('lol', 'loll', 'marSanch004', '2016-11-30 04:25:10', 134581, 0);
+('lol', 'ollo', 'sanchem', '2016-12-09 12:41:58', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -160,7 +153,7 @@ ALTER TABLE `walk_in`
 -- AUTO_INCREMENT for table `apointments`
 --
 ALTER TABLE `apointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `msgs`
 --
@@ -170,7 +163,7 @@ ALTER TABLE `msgs`
 -- AUTO_INCREMENT for table `walk_in`
 --
 ALTER TABLE `walk_in`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134582;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
